38e9bf77dc69b7943380ab3344aa5f4990bc51c8
Petrus Kambala <kmbpet001@myuct.ac.za>
2020-12-08 11:31:48 +0200

    initial commit

---
+++ src/main/kotlin/com/pet001kambala/app/NamOpsMasterFuel.kt
@@ -0,0 +1,6 @@
+package com.pet001kambala.app
+
+import com.pet001kambala.controller.HomeView
+import tornadofx.App
+
+class NamOpsMasterFuel: App(HomeView::class, Styles::class)
---
+++ src/main/kotlin/com/pet001kambala/controller/HomeView.kt
@@ -0,0 +1,10 @@
+package com.pet001kambala.controller
+
+import javafx.scene.control.Menu
+import javafx.scene.layout.BorderPane
+import tornadofx.*
+
+class HomeView : View("NamOps FuelMaster") {
+    override val root: BorderPane by fxml("/view/HomeView.fxml")
+
+}
